4e61336bb841aac0ca65d5ac76eff872322faec7
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-18 09:36:49 +0200

    Cambio de logo y numeración

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 //import { numberWithCommas2 } from './helpers';
 //import { getInTooltip, getOutTooltip, positionTooltip } from './modules/tooltip';
 import { setChartHeight } from '../modules/height';
-import { setChartCanvas, setChartCanvasImage, setCustomCanvas, setChartCustomCanvasImage } from '../modules/canvas-image';
+import { setChartCanvas, setChartCanvasImage } from '../modules/canvas-image';
 import { setRRSSLinks } from '../modules/rrss';
 import { setFixedIframeUrl } from './chart_helpers';
 
@@ -161,16 +161,12 @@ export function initChart(iframe) {
         setRRSSLinks('brecha_genero_pensiones');
 
         //Captura de pantalla de la visualización
-        //setChartCanvas();
-        setTimeout(() => {
-            setCustomCanvas();
-        }, 6000);        
+        setChartCanvas();      
 
         let pngDownload = document.getElementById('pngImage');
 
         pngDownload.addEventListener('click', function(){
-            //setChartCanvasImage('brecha_genero_pensiones');
-            setChartCustomCanvasImage('brecha_genero_pensiones');
+            setChartCanvasImage('brecha_genero_pensiones');
         });
 
         //Altura del frame
