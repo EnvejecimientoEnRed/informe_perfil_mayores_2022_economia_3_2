c14be093781e80ca523a7e36200ded0e17746a3f
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-04-27 10:07:27 +0200

    Nueva lógica de imágenes

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -12,7 +12,7 @@ const COLOR_PRIMARY_1 = '#F8B05C',
 COLOR_ANAG_PRIM_3 = '#9E3515';
 let tooltip = d3.select('#tooltip');
 
-export function initChart(iframe) {
+export function initChart() {
     //Lectura de datos
     d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_economia_3_2/main/data/brecha_genero_pensiones_europa_v2.csv', function(error,data) {
         if (error) throw error;
@@ -190,6 +190,10 @@ export function initChart(iframe) {
         //Animación del gráfico
         document.getElementById('replay').addEventListener('click', function() {
             animateChart();
+
+            setTimeout(() => {
+                setChartCanvas();
+            }, 4000);  
         });
 
         //////
@@ -203,7 +207,9 @@ export function initChart(iframe) {
         setRRSSLinks('brecha_genero_pensiones');
 
         //Captura de pantalla de la visualización
-        setChartCanvas();      
+        setTimeout(() => {
+            setChartCanvas();
+        }, 4000);      
 
         let pngDownload = document.getElementById('pngImage');
 
@@ -212,8 +218,6 @@ export function initChart(iframe) {
         });
 
         //Altura del frame
-        setChartHeight(iframe);        
-    });
-
-    
+        setChartHeight();        
+    });    
 }
