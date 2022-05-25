2c84ccecf110cbf4f45ffcba600e23437272aa3e
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 16:14:33 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -14,7 +14,7 @@ let tooltip = d3.select('#tooltip');
 
 export function initChart() {
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_economia_3_2/main/data/brecha_genero_pensiones_europa_v2.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_economia_3_2/main/data/brecha_genero_pensiones_europa_v2.csv', function(error,data) {
         if (error) throw error;
 
         // sort data
